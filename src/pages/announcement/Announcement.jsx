92937414fe073a5f96c637e00765135fae52e4e9
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2024-04-14 14:02:20 +0700

    feat(announcement): add new announcement

--- src/pages/announcement/Announcement.jsx
+++ src/pages/announcement/Announcement.jsx
@@ -1,11 +1,22 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState, useEffect } from 'react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Button,
+    TextField,
+    InputLabel,
+    InputLabelProps,
+} from '@mui/material';
 import axios from 'axios';
 import styles from '../../components/datatable/datatable_user/datatable_user.module.css';
 import ReactPaginate from 'react-paginate';
@@ -18,6 +29,8 @@ const Announcement = () => {
     const [perPage, setPerPage] = useState(6);
     const [currentPage, setCurrentPage] = useState(1);
     const [numOfTotalPages, setNumOfTotalPages] = useState(0);
+
+    const [open, setOpen] = useState(false);
 
     const loadAnnouncements = async () => {
         try {
@@ -46,6 +59,54 @@ const Announcement = () => {
         setCurrentPage(selected + 1);
     };
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const [formData, setFormData] = useState({
+        nameAnnouncement: '',
+        startAt: '',
+        note: '',
+        listEmployee: '',
+        department: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const accessToken = await AsyncStorage.getItem('accessToken');
+            await axios.post('http://localhost:5555/v1/announcement', formData, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            // Sau khi thêm mới, reset form và cập nhật dữ liệu
+            setFormData({
+                nameAnnouncement: '',
+                startAt: '',
+                note: '',
+                listEmployee: '',
+                department: '',
+            });
+            setOpen(false); // Ẩn form
+            loadAnnouncements(); // Cập nhật dữ liệu
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    };
+
     const indexOfLastAnnouncement = currentPage * perPage;
     const indexOfFirstAnnouncement = indexOfLastAnnouncement - perPage;
     const visibleAnnouncements = tableData?.slice(indexOfFirstAnnouncement, indexOfLastAnnouncement);
@@ -59,7 +120,83 @@ const Announcement = () => {
                     <div className={styles.datatable}>
                         <div className={styles.datatableTitle}>
                             <b>Lịch Biểu</b>
+                            <Button
+                                style={{ borderRadius: 5, background: 'rgb(98, 192, 216)' }}
+                                variant="contained"
+                                onClick={handleClickOpen}
+                            >
+                                Thêm mới
+                            </Button>
                         </div>
+
+                        <Dialog open={open} onClose={handleClose}>
+                            <DialogTitle>Thêm mới lịch họp</DialogTitle>
+                            <DialogContent>
+                                <form onSubmit={handleSubmit} style={{ width: '400px' }}>
+                                    <div style={{ width: '100%', marginBottom: '30px' }}>
+                                        <TextField
+                                            label="Tên cuộc họp"
+                                            type="text"
+                                            name="nameAnnouncement"
+                                            value={formData.nameAnnouncement || ''}
+                                            onChange={handleChange}
+                                            required
+                                            fullWidth
+                                        />
+                                    </div>
+                                    <div style={{ width: '100%', marginBottom: '30px', position: 'relative' }}>
+                                        <TextField
+                                            label="Ngày họp"
+                                            type="date"
+                                            name="startAt"
+                                            value={formData.startAt || ''}
+                                            onChange={handleChange}
+                                            InputLabelProps={{ shrink: true, required: true }}
+                                            required
+                                            fullWidth
+                                        />
+                                    </div>
+                                    <div style={{ width: '100%', marginBottom: '30px' }}>
+                                        <TextField
+                                            label="Ghi chú"
+                                            type="text"
+                                            name="note"
+                                            value={formData.note || ''}
+                                            onChange={handleChange}
+                                            fullWidth
+                                        />
+                                    </div>
+                                    <div style={{ width: '100%', marginBottom: '30px' }}>
+                                        <TextField
+                                            label="Nhân viên"
+                                            type="text"
+                                            name="listEmployee"
+                                            value={formData.listEmployee || ''}
+                                            onChange={handleChange}
+                                            fullWidth
+                                        />
+                                    </div>
+                                    <div style={{ width: '100%', marginBottom: '30px' }}>
+                                        <TextField
+                                            label="Phòng ban"
+                                            type="text"
+                                            name="department"
+                                            value={formData.department || ''}
+                                            onChange={handleChange}
+                                            fullWidth
+                                        />
+                                    </div>
+                                    <DialogActions>
+                                        <Button onClick={handleClose} color="primary">
+                                            Đóng
+                                        </Button>
+                                        <Button type="submit" onClick={handleSubmit} color="primary">
+                                            Thêm mới
+                                        </Button>
+                                    </DialogActions>
+                                </form>
+                            </DialogContent>
+                        </Dialog>
 
                         <TableContainer component={Paper} className={styles.table}>
                             <Table sx={{ minWidth: 1200 }} aria-label="a dense table">
